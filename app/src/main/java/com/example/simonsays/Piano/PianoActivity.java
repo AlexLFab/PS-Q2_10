e6869c40d4ef866cc32f539b9cfd3093e1044673
AdrianOjea <a.ojea.goncalves@udc.es>
2024-05-31 18:00:57 +0200

    Arreglos colores botones login y register

--- app/src/main/java/com/example/simonsays/Piano/PianoActivity.java
+++ app/src/main/java/com/example/simonsays/Piano/PianoActivity.java
@@ -94,8 +94,6 @@ public class PianoActivity extends AppCompatActivity {
                 binding.include2.iv52.setImageResource(emptyImage);
                 binding.include2.iv53.setImageResource(emptyImage);
 
-                Toast.makeText(PianoActivity.this, "Game Over", Toast.LENGTH_SHORT).show();
-
                 if (currentScore>beatScore){
                     beatScore = currentScore;
                     binding.include1.tvBeat.setText(getString(R.string.best)+ beatScore);
